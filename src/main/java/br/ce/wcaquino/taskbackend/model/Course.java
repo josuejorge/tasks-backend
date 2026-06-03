2e944a2b9ff8298152cf3b9d7ad877123436a1a6
jorgeoliveira2099 <josuejorge2099@hotmail.com>
2026-06-03 16:22:40 -0300

    corrigindo erro de serializable na classe course

--- src/main/java/br/ce/wcaquino/taskbackend/model/Course.java
+++ src/main/java/br/ce/wcaquino/taskbackend/model/Course.java
@@ -1,5 +1,6 @@
 package br.ce.wcaquino.taskbackend.model;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 import javax.persistence.Column;
@@ -9,7 +10,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Course {
+public class Course implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
